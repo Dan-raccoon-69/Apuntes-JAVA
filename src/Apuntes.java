806051c8b0418f6e51e7d6dc34c1e1ea55a5d0d9
Daniel De La Cruz <gustavodelacruz2010@gmail.com>
2022-10-18 16:18:52 -0500

    Cambios

--- src/Apuntes.java
+++ src/Apuntes.java
@@ -10,48 +10,54 @@ public class Apuntes {
         /**
          * Java es un lenguaje orientado a objetos.
          * Distribuido Multihilos
-         * Seguro e independiente de la plataforma (windows,linux, mac) con tener la JVM (java virtual machine)
+         * Seguro e independiente de la plataforma (windows,linux, mac) con tener la JVM
+         * (java virtual machine)
          */
 
         // VERSIONES DE JAVA
         /**
-         * JAVA SE: PARA PROPOSITOS GENERALES UTILIZADA EN APLICACIONES DE ESCRITORIO Y SERVIDORES.
-         * JAVA EE: PARA EL DESARROLLO DE APLICACIONES DISTRIBUIDAS QUE TRABAJAN SOBRE INTERNET.
-         * JAVA ME: PARA DISPOSITIVOS CON RECURSOS LIMITADOS TALES COMO DISPOSITIVOS MOBILES, TV, IMPRESORAS.
+         * JAVA SE: PARA PROPOSITOS GENERALES UTILIZADA EN APLICACIONES DE ESCRITORIO Y
+         * SERVIDORES.
+         * JAVA EE: PARA EL DESARROLLO DE APLICACIONES DISTRIBUIDAS QUE TRABAJAN SOBRE
+         * INTERNET.
+         * JAVA ME: PARA DISPOSITIVOS CON RECURSOS LIMITADOS TALES COMO DISPOSITIVOS
+         * MOBILES, TV, IMPRESORAS.
          */
 
         // JAVA CONCEPTOS
         /**
-         * JDK (JAVA DEVELOPMENT KIT): INCLUYE UN COMPILADOR, UN ENTORNO DE EJECUCION (RUNTIME ENVIRONMENT)Y OTRAS HERRAMIENTAS PARA EL DESARROLLO DE APLICACIONES.
+         * JDK (JAVA DEVELOPMENT KIT): INCLUYE UN COMPILADOR, UN ENTORNO DE EJECUCION
+         * (RUNTIME ENVIRONMENT)Y OTRAS HERRAMIENTAS PARA EL DESARROLLO DE APLICACIONES.
          */
-        
+
         // TIPOS DE DATOS EN JAVA (primitivos)
         /**
-            ENTEROS ******
+         * ENTEROS ******
          * byte (–128 to 127)
          * short (–32,768 to 32,767)
          * int (–2,147,483,648 a 2,147,483,647)
          * long (–9,223,372,036,854,775,808 a 9,223,372,036,854,775,807)
-            Caracteres ******
+         * Caracteres ******
          * char (cualquier caracter)
-            Deciales ******
+         * Deciales ******
          * float (debemos de colocar f al final del valor)
          * double (valores decimales muy grandes)
-            Booleanos ******
+         * Booleanos ******
          * boolean (true o false)
          */
 
         /**
          * si declaramos:
          * char letra = 97;
-        me imprimira el caracter a, ya que el 97 hace referencia a la letra minuscula a en el codigo ASCCI.
-        */
-
+         * me imprimira el caracter a, ya que el 97 hace referencia a la letra minuscula
+         * a en el codigo ASCCI.
+         */
 
         // CONSTANTES
 
         /**
-         * Una constante es aquella variable que no cambia de valor a lo largo del programa
+         * Una constante es aquella variable que no cambia de valor a lo largo del
+         * programa
          * para que sea una constante se le debe agregar la palabra final al principio.
          */
 
@@ -59,17 +65,16 @@ public class Apuntes {
         System.out.println("\n" + pi);
         System.out.println(euler + "\n");
 
+        // CASTING
 
-        //CASTING
-        
         int a = 5;
         int b = 2;
-        float c = (float)a / (float)b;
+        float c = (float) a / (float) b;
         System.out.println(c);
 
         byte a1 = 8;
         byte b1 = 2;
-        byte c1 = (byte)(a1 + b1);
+        byte c1 = (byte) (a1 + b1);
         System.out.println(c1);
 
         float a2 = 8f;
@@ -77,43 +82,43 @@ public class Apuntes {
         float c2 = a2 + b2;
         System.out.println(c2 + "\n");
 
-        //operador postIncremento preIncremento
-            // preIncremento
+        // operador postIncremento preIncremento
+        // preIncremento
         int PreIncremento = 0;
         System.out.println("PreIncremento " + ++PreIncremento);
-            // postIncremento
+        // postIncremento
         int PostIncremento = 0;
-        System.out.println("PostIncremento " + PostIncremento++ +  "\n");
+        System.out.println("PostIncremento " + PostIncremento++ + "\n");
 
-        //ejemplo
+        // ejemplo
 
-        //preIncremento
+        // preIncremento
         int preIncrementoa = 3;
         int preIncrementob = 2;
-        int preIncrementoc =  preIncrementoa * ++preIncrementob;
+        int preIncrementoc = preIncrementoa * ++preIncrementob;
         System.out.println("preIncrementoa " + preIncrementoa); // 3
         System.out.println("preIncrementob " + preIncrementob); // 3
         System.out.println("preIncrementoc " + preIncrementoc + "\n"); // 9
-        
-        //postIncremento
+
+        // postIncremento
         int postIncrementoa = 3;
         int postIncrementob = 2;
-        int postIncrementoc =  postIncrementoa * postIncrementob++;
-        //primero multipica y luego suma la unidad a postIncrementob
+        int postIncrementoc = postIncrementoa * postIncrementob++;
+        // primero multipica y luego suma la unidad a postIncrementob
         System.out.println("postIncrementoa " + postIncrementoa); // 3
         System.out.println("postIncrementob " + postIncrementob); // 3
         System.out.println("postIncrementoc " + postIncrementoc); // 9
 
         // RECOMENDACION: no utilices preIncremento ni postIncremento en una asignacion.
-        // ejemplo: int postIncrementoc =  postIncrementoa * postIncrementob++;
+        // ejemplo: int postIncrementoc = postIncrementoa * postIncrementob++;
 
         // variable = variable + 1; === variable+=1; === variable++;
 
-        
         /**
-         * Operadores aritmeticos (+,-,*,/) 
-         * atento con las jeraquia de operaciones (con prioridad de izquierda a derecha).
-        */
+         * Operadores aritmeticos (+,-,*,/)
+         * atento con las jeraquia de operaciones (con prioridad de izquierda a
+         * derecha).
+         */
 
         /**
          * Operadores logicos (AND, OR, NOT)
@@ -121,7 +126,8 @@ public class Apuntes {
          */
 
         /**
-         * Operadores relacionales (> mayor, < menor, >=  mayor o igual, <= menor o igual, == igual, != diferente)
+         * Operadores relacionales (> mayor, < menor, >= mayor o igual, <= menor o
+         * igual, == igual, != diferente)
          * siempre te dan un valor booleano.
          */
 
@@ -129,20 +135,19 @@ public class Apuntes {
          * Operador Ternario (condicion : true ? false)
          * Dependiendo a la condicion, realizara el proceso del codigo true o false.
          */
-        
-        String cadena = (8 > 9) ? "True" : "Falso"; 
-        System.out.println("\nOperador Ternario: " + cadena);
 
+        String cadena = (8 > 9) ? "True" : "Falso";
+        System.out.println("\nOperador Ternario: " + cadena);
 
         // CADENAS
         /**
          * String no es un dato de tipo primitivo, es una CLASE.
-         * podemos declararla como una variable normal o haciendo uns instancia de la clase String:
+         * podemos declararla como una variable normal o haciendo uns instancia de la
+         * clase String:
          * String variable = new String ("Texto");
          */
 
-        //  Algunos metodos de los String
-
+        // Algunos metodos de los String
         String name = "Daniel, ese es mi nombre";
         System.out.println("\nTexto: " + name);
         System.out.println("Nombre en mayusculas':" + name.toUpperCase());
@@ -153,6 +158,22 @@ public class Apuntes {
         System.out.println("Contiene 'Daniel':" + contienePalabra);
         System.out.println("Empieza con 'Daniel':" + name.startsWith("Daniel"));
         System.out.println("Termina con 'nombre':" + name.endsWith("Nombre"));
+        // con chartAt(indice) obtenemos el caracteren esa posición
+        /**
+         * Con indexof('letra') conoceremos donde empieza nuestro caracter o cadena.
+         * Si no existe retorna -1.
+         * Puedes pasarle un segundo argumento, indica el indice a partir del cual se
+         * iniciara la busqueda.
+        */
+        String mensajeTexto = "Hola Mundo";
+        int indiceLetraM = mensajeTexto.indexOf("M");
+        System.out.println("Indexof: " + indiceLetraM);
+        /**
+         * Con subString(indiceInicial,indiceFinal) nos retornara parte de  la cadena basado en los indices que le estamos mandando.
+         * Puedes pasarle un segundo argumento, indica el indiceFinal de la cadena.
+        */
+        String subString = mensajeTexto.substring(indiceLetraM, indiceLetraM + 2);
+        System.out.println("SubString: " + subString);
         // con format, format(" %s",variable de tipo String en este caso);
         String name2 = String.format("2do name: %s", "Gustavo");
         System.out.println("Usando format:" + name2);
@@ -162,77 +183,152 @@ public class Apuntes {
         String mensaje = String.format("%.2f", valorPi);
         // retorna un nuevo String
         System.out.println(mensaje);
-        // si vas a usar format solo para mandar a imprimir, es mejor usar printf(format,valor)
-        
+        // si vas a usar format solo para mandar a imprimir, es mejor usar
+        // printf(format,valor)
+
         // validar 2 cadenas
         // usamos el metodo equals
         String cadena1 = "Daniel";
         String cadena2 = "daniel";
         System.out.println("\n" + cadena1.equals(cadena2));
-        // usamos el metodo equalsIgnoreCase para que tome como iguales mayusculas como minusculas
+        // usamos el metodo equalsIgnoreCase para que tome como iguales mayusculas como
+        // minusculas
         System.out.println(cadena1.equalsIgnoreCase(cadena2));
 
-
-        //  Ciclos como for, while, do while, for each
+        // Ciclos como for, while, do while, for each
         /**
          * for(inicio, condicion, aumento-decremento){ codigo }
          * while(codicion){codigo aumento-decremento }, piensa luego procesa.
          * do {codigo aumento-decremento } while(codicion), procesa y luego piensa.
          */
 
-        //  for each
+        // for each
         System.out.println("\nFor each ejemplos:");
-        int datos[] = {1,2,3,4,5,6,7,8,9,10};
-        for(int elemento: datos){
+        int datos[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        for (int elemento : datos) {
             System.out.print(elemento + " ");
         }
         System.out.printf("\n");
-        String datos2[] = {"daniel","ana","veronica","edgar","lizbeth","antonio","luis"};
-        for(String elemento: datos2){
+        String datos2[] = { "daniel", "ana", "veronica", "edgar", "lizbeth", "antonio", "luis" };
+        for (String elemento : datos2) {
             System.out.print(elemento + " ");
         }
-        
+
         System.out.println("\n\nFor Each con mapas");
-        //for each en mapas
+        // for each en mapas
         Map<Integer, String> nombres = new HashMap<>();
-        nombres.put(1,"daniel");
-        nombres.put(2,"ana");
-        nombres.put(3,"veronica");
-        nombres.put(4,"edgar");
-        nombres.put(5,"lizbeth");
-        nombres.put(6,"antonio");
-        nombres.put(7,"luis");
-        
+        nombres.put(1, "daniel");
+        nombres.put(2, "ana");
+        nombres.put(3, "veronica");
+        nombres.put(4, "edgar");
+        nombres.put(5, "lizbeth");
+        nombres.put(6, "antonio");
+        nombres.put(7, "luis");
+
+        // recorriendo e imprimiendo
         nombres.forEach((indice, valor) -> {
             System.out.println("ID: " + indice + " Nombre: " + valor);
         });
-    
-        // ademas tenemos sentencias como BREAK y CONTINUE
+        System.out.println("");
+
+        // Ademas tenemos sentencias como BREAK y CONTINUE
         /*
-         * break: corta el proceso de un ciclo,se sale del programa sin importar si no termino las iteraciones. 
+         * break: corta el proceso de un ciclo,se sale del programa sin importar si no
+         * termino las iteraciones.
          * continue: pasa a la siguiente iteración
-        */
+         */
 
         // ARREGLOS
-        // son una coleccion de datos del mismo tipo de valor, un arreglo debe de tener un tamaño especifico.
-        // siempre necesitamos saber el tamañao para poder declarar un arreglo
+        /**
+         * Son una coleccion de datos del mismo tipo de valor, un arreglo debe de tener
+         * un tamaño especifico.
+         * Siempre necesitamos saber el tamaño para poder declarar un arreglo.
+         * Puede almacenar tipos de datos primitivos o referencias de objetos.
+         * Si almaceno enteros, solo podre almacenar enteros, si almaceno cadena
+         * entonces solo seran cadenas.
+         */
 
         // declaracion de un arreglo
 
         // arreglo lleno
-        int numeros [] = {10,9,-8,7,-6,5};
+        int numeros[] = { 10, 9, -8, 7, -6, 5, 14, 86 };
+
         // arreglo vacio con 6 espacios en memoria
         // int numeros1 [] = new int[6];
 
-        // asignarle un valor y guardarlo en nuevas variables
-        int numero1 = numeros[0] = 20;
-        int numero2 = numeros[1] = -9;
-        // mandando imprimir
+        // Para poder acceder a un valor del arreglo se necesita un indice, el indice
+        // comienza desde 0.
+        // Guardamos en numero1, el valor que hay en la posicion 0 del arreglo numeros.
+        int numero1 = numeros[0];
+        int numero2 = numeros[1];
+
+        // Asignarle un nuevo valor a un elemento del arreglo
+        numeros[1] = 69;
+
+        // Mandando imprimir
         System.out.println("Numero [0]: " + numero1);
         System.out.println("Numero [1]: " + numero2);
-        
-        
-        
+        System.out.println("Cantidad de elementos en el arreglo Numero: " + numeros.length);
+        System.out.println("Imprimiendo Arreglo: ");
+        for (int elemento : numeros) {
+            System.out.print(elemento + " ");
+        }
+
+        // ORDENACION BURBUJA
+
+        /**
+         * No es recomendado usarlo en producción, no son optimos.
+         * Es el metodo de ordenación mas facil de implementar.
+         * Adecuado para personas que empiezan a programar, entender como podemos
+         * ordenar y buscar.
+         */
+
+        // llenado con valores aleatorios
+
+        int valoresAleatorios[] = new int[10];
+        for (int i = 0; i < valoresAleatorios.length; i++) {
+            valoresAleatorios[i] = (int) ((Math.random() * 100) + 1);
+        }
+        // imprimiendo
+        System.out.println("\n\nOrdenacion burbuja\n" + "Imprimiendo Arreglo: ");
+
+        for (int valor : valoresAleatorios) {
+            System.out.print(valor + " ");
+        }
+
+        // ordenación burbuja
+        int aux = 0;
+        for (int i = 0; i < valoresAleatorios.length - 1; i++) {
+            for (int j = 0; j < valoresAleatorios.length - 1; j++) {
+                if (valoresAleatorios[j] > valoresAleatorios[j + 1]) {
+                    aux = valoresAleatorios[j + 1];
+                    valoresAleatorios[j + 1] = valoresAleatorios[j];
+                    valoresAleatorios[j] = aux;
+                }
+            }
+        }
+
+        // imprimiendo arreglo ordenado
+        System.out.println("\nOrdenados: ");
+        for (int valor : valoresAleatorios) {
+            System.out.print(valor + " ");
+        }
+
+        // Busqueda lineal
+        /**
+         * Ineficiente, ya que recorre todo el arreglo hasta encontrar el dato a buscar.
+         * Tenemos metodos mas eficientes como la busqueda binaria, pero el arreglo debe
+         * estar ordenado.
+         */
+        System.out.println("\nBusqueda: ");
+        valoresAleatorios[5] = 69;
+        int x = 69;
+        for (int i = 0; i < valoresAleatorios.length; i++) {
+            if (valoresAleatorios[i] == x) {
+                System.out.println("El numero " + x + " se encuentra en la posición " + i);
+                break;
+            }
+        }
 
     }
 }
